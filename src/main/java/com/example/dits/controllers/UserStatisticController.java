4f4866afcd8b6ac9acbf6f9c1489f392d0a00fd9
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-09-20 11:52:21 +0300

    fix sort button

--- src/main/java/com/example/dits/controllers/UserStatisticController.java
+++ src/main/java/com/example/dits/controllers/UserStatisticController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -31,6 +32,7 @@ public class UserStatisticController {
 
         List<Statistic> statisticList = statisticService.getStatisticsByUser(user);
         UserStatistics userStatistics = statisticMapper.getUserStatistics(user, statisticList);
+        Collections.sort(userStatistics.getTestStatisticList());
         session.setAttribute("userStatistics", userStatistics);
         return "user/personalStatistic";
     }
