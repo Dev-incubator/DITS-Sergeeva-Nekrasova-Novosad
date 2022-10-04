2c3f407bbc066f4016bca155a278e69bb655df0c
Anton Novosad <noobito.2011@gmail.com>
2022-10-04 15:52:32 +0300

    added fixes with display list questions in test-editor.js;

--- src/main/resources/static/js/test-editor.js
+++ src/main/resources/static/js/test-editor.js
@@ -332,7 +332,7 @@ function getQuestionHtml({name, testId, questions}) {
     </div>
     <div class="collapse question__list mt-3" id=test${testId} data-test-id=${testId}>
       ${
-        questions.reduce((accum, {questionId, description}, index) => {
+        questions.map(({questionId, description}, index) => {
             return `
             <div class="row question__item mb-2 mt-3" data-id=${questionId}>
               <span class="index-num">${index + 1}.</span>
@@ -343,7 +343,7 @@ function getQuestionHtml({name, testId, questions}) {
               </div>
             </div>
           `
-        }, '')
+        }, '').join()
     }
     </div>
   `
